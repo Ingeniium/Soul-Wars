6ac2ab222bc9aa8028e4d525a8a8d773448788e7
Andrew Olumbe <andrew.olumbe@gmail.com>
2016-12-19 23:42:33 -0600

    Add files via upload

--- Soul Wars Project (Unity v5.2)/Assets/Gun.cs
+++ Soul Wars Project (Unity v5.2)/Assets/Gun.cs
@@ -2,8 +2,84 @@
 using UnityEditor;
 using UnityEngine.UI;
 using System.Collections;
+public abstract class Item : MonoBehaviour
+{
+    T CopyComponent<T>(T original, GameObject destination) where T : Component
+    {
+        System.Type type = original.GetType();
+        Component copy = destination.AddComponent(type);
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+        foreach (System.Reflection.FieldInfo field in fields)
+        {
+            field.SetValue(copy, field.GetValue(original));
+        }
+        return copy as T;
+    }
+    protected string name;
+    public Canvas drop_canvas;
+    protected Canvas drop_canvas_show;
+    protected bool dropped = false;
+    public GameObject item_image;
+    protected GameObject _item_image;
+    public GameObject asset_reference;
+    public void DropItem(ref double chance)
+    {
+        System.Random rand= new System.Random();
+        if (rand.NextDouble() <= chance)
+        {
+            gameObject.layer = 0;
+            transform.parent = null;
+            gameObject.AddComponent<Rigidbody>();
+            gameObject.AddComponent<BoxCollider>();
+            drop_canvas_show = Instantiate(drop_canvas, transform.position + new Vector3(0,0,.5f), drop_canvas.transform.rotation) as Canvas;
+            drop_canvas_show.GetComponentInChildren<Text>().text = name;
+            Destroy(drop_canvas_show.gameObject, 1f);
+            dropped = true;
+        }
+    }
+    protected void RetrieveItem()
+    {
+       _item_image = Instantiate(item_image, item_image.transform.position, item_image.transform.rotation) as GameObject;
+       CopyComponent<Item>(this,_item_image);
+       _item_image.GetComponentInChildren<ItemImage>().item_script = _item_image.GetComponent<Item>();
+       _item_image.GetComponentInChildren<ItemImage>().Item = asset_reference;
+       GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>().InsertItem(ref _item_image);
+     
+    }
+    void OnMouseEnter()
+    {
+        if (dropped)
+        {
+            drop_canvas_show = Instantiate(drop_canvas, transform.position + new Vector3(0, 0, .5f), drop_canvas.transform.rotation) as Canvas;
+            drop_canvas_show.GetComponentInChildren<Text>().text = name;
+        }
+    }
 
-public class Gun : MonoBehaviour {
+    void OnMouseDown()
+    {
+        if (dropped)
+        {
+            print("retrieve");
+            RetrieveItem();
+            if (drop_canvas_show)
+            {
+                Destroy(drop_canvas_show.gameObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (drop_canvas_show)
+        {
+            Destroy(drop_canvas_show.gameObject);
+        }
+    }
+    
+}
+
+public class Gun : Item {
 	public GameObject Bullet;
 	private GameObject bullet;
 	public float reload_time = 1.0f;
@@ -32,6 +108,11 @@ public class Gun : MonoBehaviour {
     }
     public override string ToString()
     {
-        return string.Format("Launches a powerful arrow.\n Homing : {0} Damage : {1} \n Reload Time : {2}", home_radius, damage, reload_time);                
-    }
+        return string.Format(name + "Launches a powerful arrow.\n Homing : {0} Damage : {1} \n Reload Time : {2}", home_radius, damage, reload_time);                
+    }
+    void Start()
+    {
+        name = "Basic \n";
+    }
+    
  }
